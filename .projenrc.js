68dbb5ce50a9abce7b5f33a344618c0760ec8536
dmytro.kosiachenko <dmytro.kosiachenko@xl2.de>
2022-08-21 12:04:08 +0200

    wip

--- .projenrc.js
+++ .projenrc.js
@@ -5,9 +5,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'dmytro.kosiachenko@xl2.de',
   cdkVersion: '2.37.0',
   defaultReleaseBranch: 'main',
-  name: 'budget-alerts',
-  repositoryUrl: 'https://github.com/dmytro.kosiachenko/budget-alerts.git',
-  deps: ['aws-cdk-lib/aws-budgets', 'aws-cdk-lib/aws-sns'],
+  name: 'cdk-budget-alerts',
+  repositoryUrl: 'https://github.com/dmytrokosiachenko/cdk-budget-alerts.git',
+  autoMerge: false,
 
 });
 project.synth();
