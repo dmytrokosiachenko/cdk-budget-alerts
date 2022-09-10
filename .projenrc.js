5cb361cb4bdb7cfda58782d56d33e1b0636b09f0
dmytro.kosiachenko <dmytro.kosiachenko@xl2.de>
2022-09-10 10:29:27 +0200

    remove dependabot

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'dmytro.kosiachenko@xl2.de',
   cdkVersion: '2.37.0',
   defaultReleaseBranch: 'main',
-  dependabot: true,
   name: 'cdk-budget-alerts',
   repositoryUrl: 'https://github.com/dmytrokosiachenko/cdk-budget-alerts.git',
   autoMerge: false,
